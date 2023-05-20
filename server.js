28801e6a0c80f36ab46315750b55d1363a8eff68
Shekhar2353 <shekharankoori2353@gmail.com>
2023-05-20 15:19:39 +0530

    confiq to config

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./confiq/db");
+const mongoose = require('mongoose')
+// const connectDB = require("./config/db");
 const app = express();
 const port = 3001;
 
-connectDB();
+// connectDB();
+var url = "mongodb+srv://Calc:uYN5iw2S2a70TW76@cluster0.hlnpp.mongodb.net/firstapp";
+// var url = "mongodb://Calc:uYN5iw2S2a70TW76@main-shard-00-00-03xkr.mongodb.net:27017,main-shard-00-01-03xkr.mongodb.net:27017,main-shard-00-02-03xkr.mongodb.net:27017/main?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
+// var url = "mongodb://localhost:27017/farmtoconsumer"
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(url)
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+connectDB()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
